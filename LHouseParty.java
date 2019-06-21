78b2ea4922f350a9fdd7d049342e7fcffba54636
manniqueen <38429726+manniqueen@users.noreply.github.com>
2019-06-21 14:51:42 +0300

    Add files via upload

---
+++ LHouseParty.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LHouseParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> people =new ArrayList<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (n -- > 0){
+            String command = scanner.nextLine();
+            String [] tokens = command.split(" +");
+            String name = tokens[0];
+
+            if (command.contains(" is going")){
+                if (people.contains(name)) {
+                    System.out.println(name + " is already in the list!");
+                }else {
+                    people.add(name);
+                }
+
+
+
+            }else {
+                if (!people.remove(name)){
+                    System.out.println(name + " is not in the list!");
+
+                }
+                
+            }
+        }
+
+        for (String person : people) {
+            System.out.println(person);
+            
+        }
+    }
+}
